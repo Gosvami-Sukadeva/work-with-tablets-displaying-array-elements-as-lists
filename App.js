ec196a795738d992cf5b2953849d84eb1e74ae15
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-04-01 20:00:28 +0200

    using template string

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
+const Item = props => <li>{`owoc ${props.content}`}</li>;
+
 class ListItems extends React.Component {
   state = {
     items: ["jabłko", "śliwka", "gruszka"]
   };
   render() {
-    return (
-      <ul>
-        {this.state.items.map(item => (
-          <li key={item}>{`owoc ${item}`}</li>
-        ))}
-      </ul>
-    );
+    const Items = this.state.items.map(item => (
+      <Item key={item} content={item} />
+    ));
+    return <ul>{Items}</ul>;
   }
 }
 
