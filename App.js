0943a2495f87dae865557f3f5e86adbce4b43e24
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-04-04 13:56:43 +0200

    work with data in objects

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
-const Item = props => <li>{`owoc ${props.content}`}</li>;
+const data = {
+  users: [
+    {
+      id: 1,
+      age: 29,
+      name: "Arek",
+    },
+    {
+      id: 2,
+      age: 49,
+      name: "Marta",
+    },
+    {
+      id: 3,
+      age: 19,
+      name: "Stasia",
+    },
+  ],
+};
+
+const Item = ({ user }) => (
+  <div>
+    <h1>Użytkownik {user.name}</h1>
+    <h2>Ma {user.age}</h2>
+  </div>
+);
 
 class ListItems extends React.Component {
-  state = {
-    items: ["jabłko", "śliwka", "gruszka"]
-  };
+  // state = {
+  //   items: ["jabłko", "śliwka", "gruszka"],
+  // };
   render() {
-    const Items = this.state.items.map(item => (
-      <Item key={item} content={item} />
-    ));
+    const users = this.props.data.users;
+    const Items = users.map((user) => <Item key={user.id} user={user} />);
     return <ul>{Items}</ul>;
   }
 }
 
-ReactDOM.render(<ListItems />, document.getElementById("root"));
+ReactDOM.render(<ListItems data={data} />, document.getElementById("root"));
