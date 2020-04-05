5f0bc83a2db2082e5873bb455166f1a6251f8e89
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-04-05 21:10:55 +0200

    sorting by gender

--- App.js
+++ App.js
@@ -4,34 +4,46 @@ const data = {
       id: 1,
       age: 29,
       name: "Arek",
+      sex: "male",
     },
     {
       id: 2,
       age: 49,
       name: "Marta",
+      sex: "female",
     },
     {
       id: 3,
       age: 19,
       name: "Stasia",
+      sex: "female",
+    },
+    {
+      id: 4,
+      age: 24,
+      name: "Karol",
+      sex: "male",
     },
   ],
 };
 
 const Item = ({ user }) => (
-  <div>
-    <h1>Użytkownik {user.name}</h1>
-    <h2>Ma {user.age}</h2>
+  <div className="userInfo">
+    <h1>{user.name}</h1>
+    <p>Informacje o użytkowniku</p>
+    <p>
+      Wiek użytkownika: <strong>{user.age}</strong>
+    </p>
+    <p>Płeć użytkownika: {user.sex}</p>
   </div>
 );
 
 class ListItems extends React.Component {
-  // state = {
-  //   items: ["jabłko", "śliwka", "gruszka"],
-  // };
+  state = {};
   render() {
-    const users = this.props.data.users;
-    const Items = users.map((user) => <Item key={user.id} user={user} />);
+    let users = this.props.data.users;
+    users = users.filter((user) => user.sex === "female"); //jeśli false to tablica się nie pojawi i za pomocą metody map utworzy nową
+    const Items = users.map((user) => <Item key={user.id} user={user} />); //key ma być stworzony gdzie jest map a nie na górze gdzie jest li
     return <ul>{Items}</ul>;
   }
 }
